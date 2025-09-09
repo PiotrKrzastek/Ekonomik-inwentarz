dae67157ef9657892800aa292584905eacdd3ae4
Piotr Krząstek <132817321+PiotrKrzastek@users.noreply.github.com>
2025-09-09 14:34:51 +0200

    Update settings.py
    Changed login toggle to be bool

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 ROOT_LOGINS = ["pkrzastek@ekonomik.gniezno.pl"] # Default users allowed to log in
 SESSION_TYPE = "filesystem"
-LOGIN_ENABLED = os.getenv("LOGIN_REQUIRED")
+LOGIN_ENABLED = bool(os.getenv("LOGIN_REQUIRED"))
 SECRET_KEY = os.getenv("SECRET_KEY")
